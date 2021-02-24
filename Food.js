2c51d05f8534bc5fb0cd6cb299a3bc1ef477f8d8
tanay6june <72078342+tanay6june@users.noreply.github.com>
2021-02-24 18:39:28 +0530

    Add files via upload

--- Food.js
+++ Food.js
@@ -1,24 +1,43 @@
 class Food {
-    constructor( 
-        FoodStock,
-        ){}
-  
-  display(){
-   var x=50,y=100
-  
-   imageMode(CENTRE);
-   image(this.image,720,220,70,70);
+  constructor(){
+  this.foodStock=0;
+  this.lastFed;
+  this.image=loadImage('Images/Milk.png');
+  }
 
-   if(this.foodStock!=0){
-       for(var i=10;i<this.foodStock;i++){
-          if(i%10==0) {
-            x=80;
-            y=y+50;
-          }
-          image(this.image,x,y,50,50)
-          x=x+30
-       }
+ updateFoodStock(foodStock){
+  this.foodStock=foodStock;
+ }
+
+ getFedTime(lastFed){
+   this.lastFed=lastFed;
+ }
+
+ deductFood(){
+   if(this.foodStock>0){
+    this.foodStock=this.foodStock-1;
    }
+  }
 
-  }      
+  getFoodStock(){
+    return this.foodStock;
+  }
+
+  display(){
+    var x=80,y=100;
+    
+    imageMode(CENTER);
+    image(this.image,720,220,70,70);
+    
+    if(this.foodStock!=0){
+      for(var i=0;i<this.foodStock;i++){
+        if(i%10==0){
+          x=80;
+          y=y+50;
+        }
+        image(this.image,x,y,50,50);
+        x=x+30;
+      }
+    }
+  }
 }
